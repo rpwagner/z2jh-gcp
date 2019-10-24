8c60ed6e59a9019b04dafe78de858f25e831e8e4
Rick Wagner <rick@globus.org>
2019-10-23 22:51:14 -0500

    override KubeSpawner

--- start-gcp.sh
+++ start-gcp.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-python /srv/setup-gcp.py
-nohup /opt/globusconnectpersonal-2.3.9/globusconnectpersonal -start &
+if [ ! -z "$CREATEGCP" ]
+then
+    if [ "$CREATEGCP" -eq "1" ]
+    then
+        python /srv/setup-gcp.py
+        nohup /opt/globusconnectpersonal-2.3.9/globusconnectpersonal -start &
+    fi
+fi
