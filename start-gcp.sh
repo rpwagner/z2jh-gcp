78c34c3a7ff6653adfc2cb6ca395860d2f759e1d
rpwagnerbuilds <56850371+rpwagnerbuilds@users.noreply.github.com>
2019-10-24 17:53:23 -0500

    limit permissions on GCP dir

--- start-gcp.sh
+++ start-gcp.sh
@@ -5,6 +5,7 @@ then
     if [ "$CREATEGCP" -eq "1" ]
     then
         python /srv/setup-gcp.py
+        chmod -R go-rwx /home/jovyan/.globusonline
         nohup /opt/globusconnectpersonal-2.3.9/globusconnectpersonal -start &
     fi
 fi
